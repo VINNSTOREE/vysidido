b793117b4d9a68dacfe818afbf713c2341b5b006
VINNSTOREE <vingg1633@gmail.com>
2025-07-08 14:08:21 +0700

    Add files via upload

---
+++ vinnapi-deposit/server.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const path = require('path');
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+const depositRoutes = require('./routes/deposit');
+const ppobRoutes = require('./routes/ppob');
+
+app.use('/api', depositRoutes);
+app.use('/api', ppobRoutes);
+
+app.listen(3000, () => console.log('Running at http://localhost:3000'));
